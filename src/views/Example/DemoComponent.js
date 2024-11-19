d3e17af39fd6a717963e747e40a3c6fceeb6a7dd
chinhfree <chinhquy111@gmail.com>
2024-11-19 23:27:57 +0700

    react.fragment, keyword: this, example event

--- src/views/Example/DemoComponent.js
+++ src/views/Example/DemoComponent.js
@@ -9,11 +9,37 @@ JSX --> javascript XML  --> render HTML
 */
 
 class DemoComponent extends React.Component {
+    state = {
+        name: 'ChinhFree',
+        website: 'chinh.free'
+    }
+    myvar = {
+        name: 'testname',
+        website: 'test website'
+    }
+    //example event
+    handleOnchangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
     render() {
-        let name = 'ChinhFree';
+        //keyword use to get value in state: this
         return (
-            <h2>My name is {name} and here is my demo component</h2>
-        );
+            <>
+            <input value={this.state.name} type='text' 
+                onChange={(event) => this.handleOnchangeName(event)}
+            />
+                <div className='firstDiv'> {this.state.name}. Hello React</div>
+                <div className='secondDiv'>My website {this.state.website}</div>
+                <div className='firstDiv'>My name is {this.myvar.name}. Hello React</div>
+                <div className='secondDiv'>My website {this.myvar.website}</div>
+                
+            </>
+            
+                
+            
+        )
     }
 }
 
