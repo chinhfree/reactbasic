06ab9c39cc15a010ecb42f2cd7f18010674622e0
chinhfree <chinh@cung.pro>
2024-11-21 15:30:40 +0700

    stateless - statefull component/ function vs class component

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -8,72 +8,54 @@ JSX --> javascript XML  --> render HTML
 
 */
 
-class ChildComponent extends React.Component {
-    // state = {
-    //     name: 'ChinhFree',
-    //     website: 'chinh.free'
-    // }
-    // myvar = {
-    //     name: 'testname',
-    //     website: 'test website'
-    // }
-    // //example event
-    // handleOnchangeName = (event) => {
-    //     this.setState({
-    //         name: event.target.value
-    //     })
-    //     // console.log("event: ", event)
-    //     // console.log("event.target: ", event.target)
-    //     // console.log("event.target.value: ", event.target.value)
-    // }
-    // handleOnClick = () => {
-    //     alert('Clicked!');
-    //     console.log('hit the button')
-    // }
-    state = {
-        firstName: '',
-        lastName: ''
-    }
-    handleChangeFirstName = (event) => {
-        this.setState({
-            firstName: event.target.value
-        })
-    }
-    handleChangeLastName = (event) => {
-        this.setState({
-            lastName: event.target.value
-        })
-    }
-    handleSubmit = (event) => {
-        event.preventDefault()
-        console.log(this.state)
-    }
-    render() {
-        //keyword use to get value in state: this
+// class ChildComponent extends React.Component {
+//     render() {
+//         let { name, age, address, jobs } = this.props;
+//         return (
+//             <>
+//                 <div>
+//                     child component {name} - {age} - {address}
+//                 </div>
+//                 <div className='jobs-list'>
+//                     {
+//                         jobs.map((item, index) => {
+//                             return (
+//                                 <div key={item.id}>
+//                                     {item.title} - {item.salary}
+//                                 </div>
+//                             )
+//                         })
+//                     }
+//                 </div>
 
-        //destructuring assignment - variable names must be the same as props.keys
-        let { name, age, address, jobs } = this.props;
-        return (
-            <>
-                <div>
-                    child component {name} - {age} - {address}
-                </div>
-                <div className='jobs-list'>
-                    {
-                        jobs.map((item, index) => {
-                            return (
-                                <div key={item.id}>
-                                    {item.title} - {item.salary}
-                                </div>
-                            )
-                        })
-                    }
-                </div>
+//             </>
 
-            </>
+//         )
+//     }
+// }
 
-        )
-    }
+//function component
+const ChildComponent = (props) => {
+    let { name, age, address, jobs } = props;
+    return (
+        <>
+            <div>
+                child component {name} - {age} - {address}
+            </div>
+            <div className='jobs-list'>
+                {
+                    jobs.map((item, index) => {
+                        return (
+                            <div key={item.id}>
+                                {item.title} - {item.salary}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+        </>
+    )
 }
 
 export default ChildComponent;
