e53b1d7153464a9e66f886806fe00085df848dde
chinhfree <chinh@cung.pro>
2024-11-25 16:17:01 +0700

    HOC - higher order component

--- src/views/nav/Home.js
+++ src/views/nav/Home.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { withRouter } from "react-router";
+import Color from '../HOC/Color';
 
 class Home extends React.Component {
+    componentDidMount() {
+        setTimeout(() => {
+            this.props.history.push('/todo')
+        }, 3000)
+    }
+
     render() {
         return (
             <div>Hello World</div>
@@ -8,4 +16,5 @@ class Home extends React.Component {
     }
 }
 
-export default Home;
+// export default withRouter(Home);
+export default Color(Home);
